6c4af70f8a060346813b388981d589a019a4be8b
Reaper47i <vineetbhengra96@gmail.com>
2022-06-23 22:30:45 +0530

    basic nav and landing page done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// function setMob() {
+//   let width = window.innerWidth;
+//   if (width <= 679) {
+//     document.getElementById("navContents").className = "mobNavC";
+//   } else {
+//     document.getElementById("navContents").className = "normalNavC flexCenter";
+//   }
+// }
 
+function resizeWindow() {
+  let width = window.innerWidth;
+  if (width <= 679) {
+    document.getElementById("navContents").className = "mobNavC";
+  } else {
+    document.getElementById("navContents").className = "normalNavC flexCenter";
+  }
+}
+// addEventListener("DOMContentLoaded", setMob);
+addEventListener("resize", resizeWindow);
